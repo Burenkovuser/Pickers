47739aa4ebd76e41532e0ba311f4af9c32f6800b
Burenkovuser <nb2207@yandex.ru>
2017-05-27 18:39:00 +0700

    Initial Commit

---
+++ Pickers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pickers
+//
+//  Created by Vasilii on 27.05.17.
+//  Copyright © 2017 Vasilii Burenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pickers/DoubleComponentPickerViewController.h
@@ -0,0 +1,13 @@
+//
+//  DoubleComponentPickerViewController.h
+//  Pickers
+//
+//  Created by Vasilii on 27.05.17.
+//  Copyright © 2017 Vasilii Burenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DoubleComponentPickerViewController : UIViewController
+
+@end
